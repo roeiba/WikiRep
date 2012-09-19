1da8733575756bbea2aa03bf6c63a6cfe2efd2ed
Roei Bar Aviv <roeib@infinidat.com>
2012-09-19 18:37:51 +0300

    Introduce WordData class

---
+++ src/worflow.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 19, 2012
+
+@author: roeib
+'''
+
+
+# learning data:
+#     collection of articles: when each article represents different concept
+#    
+
+# compare input:
+#    text_1, text_2: two text which should be compared
+#                    each text is an list of words
+     
+     
+# Run at once flow
+# -----------------
+
+#      
