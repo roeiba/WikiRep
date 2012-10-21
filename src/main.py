8557c570e27be3caca9b05841f2b37b10e479150
Roei Bar Aviv <roeib@infinidat.com>
2012-10-21 20:05:58 +0200

    Created parser

--- src/main.py
+++ src/main.py
@@ -1,60 +1,58 @@
-from model.semantic_interpreter import SemanticInterpreter
-from model import math_utils
-filepath = ""
-text_1 = ""
-text_2 = ""
+#!/usr/bin/env python2.7
+from model.wiki_knowledge import WikiKnowledge
+import sys
+    
+class colors(object):
+    BLUE = '\x1b[34m'
+    RESET = '\x1b[39m'
+    YELLOW = '\x1b[33m'
+    GREEN = '\x1b[32m'
+    WHITE = '\x1b[37m'
+    CYAN = '\x1b[36m'
+    BLACK = '\x1b[30m'
+    MAGENTA = '\x1b[35m'
+    RED = '\x1b[31m'
+    
+    
+def create_argument_parser():
+    from argparse import ArgumentParser
 
-'''
+    parser = ArgumentParser(description='wikipedia dump creator. ')
+    parser.add_argument("--articles", nargs="*", default=['Knowledge'],
+                        help="Download specified articles from wikipedia site," + 
+                        "merges them into one file and compress it as wikipedia dump file " )
+    
+    parser.add_argument("--logs", action='store_true', default=False,
+                        help="Save logs. Stores logs on local server")
+        
+    return parser
 
-config.yml
-
-workspace/
-    production/
+def _error(msg):
+    print("{}{}{}".format(colors.RED, msg, colors.RESET))
     
-    active_space/
-        wikidumb.bz2
-        wikiparsed.xml
-        wikibuild.wdb
-    spaces/
-        tests/
-        ivan_try
-    users/
-
-wikiknows --makedump article1 [article2] [...] [-dump wikidump.bz2]
-    downloads specified articles from wikipedia site, 
-    merges them into one file, compreses it as wikipedia dump file
-    article1 - article canonic name on wikipedia webpage
-    -dump - output filename if not specified [[defaults]] are used
+def print_failure_info():
+    _error("\n")
+    _error("-"*80)
+    _error("wikiknow execution failed!")
+    _error('Additional information can be at our site: https://github.com/roeiba/WikiRep/wiki')
     
-wikiknows --download [-form url] [-to wikidump.bz2]
-    downloads wikipedia dump file  from wikipedia site,
-    -form:  url of dump file, if not specified [[defaults]] are used
-    -to:    dump filepath on disk, if not specified [[defaults]] are used
+def main(argv=None):
+    try:
+        print "Starting..."
+        if argv is None:
+            argv = sys.argv[1:]
+        parser = create_argument_parser()
+        args = parser.parse_args()
+        print "Finished."        
     
-wikiknows --parse  [-dump wikidump.bz2] [-to wikiparsed.xml]
-wikiknows --bulid [-from wikiparsed.xml] [-to wikibuild.wdb]
-wikiknows --compare [-text text1.txt] [-with text2.txt] [-using wikibuild.wdb]
-wikiknows --wikivalue [-text text1.txt] [-using wikibuild.wdb]
-wikiknows --analize  [-text text1.txt] [-using wikibuild.wdb]
-'''
-def main():
-    pass
-    #load database 
-    #database = utilites.load_db_from_file(filepath)
-    #database = utils.load_db_stub(filepath)
+    except SystemExit, e:
+        return e.code
+    except:
+        import traceback
+        traceback.print_exc()
+        print_failure_info()
+        return 2
+    return 0
     
-    #init interpreter
-    #semantic_interpreter = SemanticInterpreter(database)
- 
-    #generate weighted vectors
-    #w_vector_1 = semantic_interpreter.build_weighted_vector(text_1) 
-    #w_vector_2 = semantic_interpreter.build_weighted_vector(text_2)
-    
-    #compare vectors
-    #correlation = math_utils.cosine_metrics(w_vector_1, w_vector_2)
-    #print "correlation is: {}".format(correlation)
-    
-if __name__ == '__main__':
-    main() 
-    
-    
+if __name__ == "__main__":
+    sys.exit(main())
