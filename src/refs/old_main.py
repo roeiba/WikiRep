7ee0aa51e5fada49151e2885ca99ab6bcaffd244
Roei Bar Aviv <roeib@infinidat.com>
2012-10-21 20:03:43 +0200

    Move old main to refs/

---
+++ src/refs/old_main.py
@@ -0,0 +1,34 @@
+
+'''
+
+config.yml
+
+workspace/
+    production/
+    
+    active_space/
+        wikidumb.bz2
+        wikiparsed.xml
+        wikibuild.wdb
+    spaces/
+        tests/
+        ivan_try
+    users/
+'''
+
+
+#load database 
+#database = utilites.load_db_from_file(filepath)
+#database = utils.load_db_stub(filepath)
+
+#init interpreter
+#semantic_interpreter = SemanticInterpreter(database)
+
+#generate weighted vectors
+#w_vector_1 = semantic_interpreter.build_weighted_vector(text_1) 
+#w_vector_2 = semantic_interpreter.build_weighted_vector(text_2)
+
+#compare vectors
+#correlation = math_utils.cosine_metrics(w_vector_1, w_vector_2)
+#print "correlation is: {}".format(correlation)
+
