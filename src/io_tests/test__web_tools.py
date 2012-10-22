f30d18026a1237215e61894768528cc5a41e5184
Roei Bar Aviv <roeib@infinidat.com>
2012-10-22 20:20:39 +0200

    Rename import wt -> webtools

--- src/io_tests/test__web_tools.py
+++ src/io_tests/test__web_tools.py
@@ -4,7 +4,8 @@ Created on Oct 15, 2012
 @author: inesmeya
 '''
 import unittest
-import parsers.web_tools as wt
+import parsers.web_tools as webtools
+import parsers.parse_tools as parser
 import StringIO
 import xml.etree.ElementTree as etree
 import os
@@ -16,7 +17,7 @@ class TestGetXmlPage(unittest.TestCase):
         self.assertIsNotNone(r)
 
     def test_urllib(self):
-        self.gen_test_wiki_xmlpage(wt.get_wiki_xmlpage_urllib)
+        self.gen_test_wiki_xmlpage(webtools.get_wiki_xmlpage_urllib)
 
     def test_wget(self):
         #we don't expect to find wget on windows station
@@ -24,10 +25,10 @@ class TestGetXmlPage(unittest.TestCase):
         osname = os.name.lower()
         if osname.find('win') > -1 or osname.find('nt') > -1:
             return
-        self.gen_test_wiki_xmlpage(wt.get_wiki_xmlpage_wget)
+        self.gen_test_wiki_xmlpage(webtools.get_wiki_xmlpage_wget)
 
     def test_get_wiki_xmlpage(self):
-        self.gen_test_wiki_xmlpage(wt.get_wiki_xmlpage)
+        self.gen_test_wiki_xmlpage(webtools.get_wiki_xmlpage)
 
 
 class TestMakeDump(unittest.TestCase):
@@ -51,7 +52,7 @@ class TestMakeDump(unittest.TestCase):
     def test__string_dump__number_of_pages(self):
         titles = ['Ross_Ice_Shelf', 'Southern_Cross_Expedition', 'Ice_shelf']
         output = StringIO.StringIO()
-        wt.make_articles_dump(titles, output)
+        webtools.make_articles_dump(titles, output)
         content = output.getvalue()
         output.close()
         self.assertEqual(content.count('<page>'), len(titles))
@@ -59,20 +60,22 @@ class TestMakeDump(unittest.TestCase):
     def test__string_dump__valid_xml(self):
         titles = ['Ross_Ice_Shelf', 'Southern_Cross_Expedition', ' Ice_shelf']
         output = StringIO.StringIO()
-        wt.make_articles_dump(titles, output)
+        webtools.make_articles_dump(titles, output)
         content = output.getvalue()
         output.close()
 
         root = etree.fromstring(content)
-        pages = root.findall(wt.mk_tag('page'))
+        pages = root.findall(webtools.mk_tag('page'))
 
         self.assertEqual(len(pages), len(titles))
 
+
     def test__articles_dump_to_file(self):
+        
         titles = ['Ross_Ice_Shelf', 'Southern_Cross_Expedition', 'Ice_shelf']
-        wt.articles_dump_to_file(titles, 'test.xml.tmp')
-        root = etree.parse('test.xml.tmp')
-        pages = root.findall(wt.mk_tag('page'))
+        webtools.articles_dump_to_file(titles, 'test.tmp')
+        root = parser.parse('test.tmp.xml') 
+        pages = root.findall(webtools.mk_tag('page'))
         self.assertEqual(len(pages), len(titles))
 
 if __name__ == "__main__":
