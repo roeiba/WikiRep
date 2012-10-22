0131d3415a1c81317722404a84ff90d886c90949
Roei Bar Aviv <roeib@infinidat.com>
2012-10-22 20:23:46 +0200

    Implement wikiKnowledge

--- src/model/wiki_knowledge.py
+++ src/model/wiki_knowledge.py
@@ -1,51 +1,82 @@
+from model.db_builder import DbBuilder
+from model import stop_words_stemmer
+from model.wiki_doc import WikiDocument
+from model.semantic_interpreter import SemanticInterpreter
+from model import math_utils
+from parsers import web_tools
+from parsers import parse_tools
 
 class WikiKnowledge(object):
+    def __init__(self, 
+                 stemmer=None, 
+                 compare_method=None):
+        if compare_method is None: compare_method = math_utils.cosine_metrics
+        self.default_compare = compare_method
+        if stemmer is None: stemmer = stop_words_stemmer.get_default_stemmer()
+        self.default_stemmer = stemmer
+        
+    def make_dump(self, wiki_dump, compressed=False, *articles):
+        """ Download specified articles from Wikipedia site, 
+            merges them into one file, compresses it as Wikipedia dump file
+            @param articles: article's canonic name on Wikipedia web page
+            @param wiki_dump: output filename (if not specified default is used)
+        """
+        web_tools.articles_dump_to_file(articles, wiki_dump, compressed=compressed)
     
-    def make_dump(self, wiki_dump, *articles):
+    def download_all(self, wiki_dump=None):
+        """ Download whole wikipedia into dump file.
+            @param src_url: url of dump file (if not specified default is used)
+            @param wiki_dump: output dump filename, etc. wikidump.bz2 (if not specified default is used)
         """
-        Download specified articles from Wikipedia site, 
-        merges them into one file, compresses it as Wikipedia dump file
-        @param articles: article's canonic name on Wikipedia web page
-        @param wiki_dump: output filename (if not specified default is used)
-        """
-        pass
-    
-    def download(self, src_url, wiki_dump=None):
-        """
-        Download wikipedia dump file.
-        @param src_url: url of dump file (if not specified default is used)
-        @param wiki_dump: dump filename, etc. wikidump.bz2 (if not specified default is used)
-        """
-        pass
+        raise Exception("Not yet implemented")
     
     def parse(self, wiki_dump, output):
+        """ Parses wiki_dump.
+            @param wiki_dump: input wikipedia dump filename  
+            @param output: output xml filename
         """
-        Parses wiki_dump.
-        @param wiki_dump: input wikipedia dump filename  
-        @param output: output xml filename
+        #TODO: define and implement
+        raise Exception("Not yet implemented - what exactly this method do?")
+        
+    def build(self, src, output, stemmer=None):
+        """ builds WikiRep database.
+            @param src: Wikipedia source xml (etc. wikiparsed.xml)
+            @param output: WikiRep database output file
         """
-        pass
-    def build(self, src, output):
+        if stemmer is None: stemmer = self.default_stemmer
+        db_builder = DbBuilder(stemmer)
+        
+        for doc_id, title, text, rev_id in parse_tools.extract_pages(src):
+            doc = WikiDocument(doc_id=doc_id, title=title, raw_text=text, rev_id=rev_id)
+            db_builder.add_document(doc)
+        db = db_builder.build()
+        self.semantic_intepreter = SemanticInterpreter(db, stemmer)
+            
+    def compare(self, text1, text2, compare_method=None):
         """
-        builds WikiRep database.
-        @param src: Wikipedia source xml (etc. wikiparsed.xml)
-        @param output: WikiRep database output file
+            Compares two texts in wikipedia space.
+            @param db: WikiRep database filename
         """
-        pass
-    def compare(self, text1, text2, db):
+        #semantic_interpreter = SemanticInterpreter(database)
+        #generate weighted vectors
+        w_vector_1 = self.get_text_value(text1)
+        w_vector_2 = self.get_text_value(text1)
+        
+        #compare vectors
+        if compare_method is None: compare_method = self.default_compare
+        correlation = compare_method(w_vector_1, w_vector_2)
+        print "correlation is: {}".format(correlation)
+        return correlation
+        
+    def get_text_value(self, text):
         """
-        Compares two texts in wikipedia space.
-        @param db: WikiRep database filename
+            returns the text vector in wikipedia space.
         """
-        pass
-    def get_text_value(self, text, db):
-        """
-        returns the text vector in wikipedia space.
-        @param db: WikiRep database filename
-        """
-        pass
+        return self.semantic_intepreter.build_weighted_vector(text)
+    
     def analize(self, text1, db):
         """
-        @param db: WikiRep database filename
+            #TODO: Define method and implement
+            @param db: WikiRep database filename
         """
         pass
