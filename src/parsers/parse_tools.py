e366b3f0bff5c084594ece4babf43f22b64c2ab4
Roei Bar Aviv <roeib@infinidat.com>
2012-10-23 18:46:13 +0200

    Implement parse_clean which generates clean text

--- src/parsers/parse_tools.py
+++ src/parsers/parse_tools.py
@@ -6,6 +6,7 @@ Created on Oct 15, 2012
 
 import xml.etree.ElementTree as etree
 import parsers.web_tools as wt
+import WikiExtractor 
 import gzip
 
 _PAGE_TAG = wt.mk_tag('page')
@@ -54,6 +55,17 @@ def extract_pages(f):
             elem.clear()     
             root.clear()
 
+def extract_clean_pages(src_wiki_dump, keep_sections=False,keep_links=False):
+    """ Wrapper for extract_pages that output clean text according to parameters
+    @param keep_sections: preserve sections
+    @param keep_links: preserve links
+    """
+    for tid, title, text, rev_id in extract_pages(src_wiki_dump):
+        clean_text =  WikiExtractor.run(text,keep_sections=keep_sections,keep_links=keep_links)
+        
+        yield tid, title, clean_text, rev_id
+        
+        
 def parse(filename): 
     if filename.endswith(".gzip"):
         filename = gzip.open(filename)
