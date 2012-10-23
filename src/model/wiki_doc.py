1b4755a7c1b9b9b0c2b5dad48c7f31e842c5a4d7
Roei Bar Aviv <roeib@infinidat.com>
2012-10-23 18:42:48 +0200

    Add __str__ and logging to WikiDocment

--- src/model/wiki_doc.py
+++ src/model/wiki_doc.py
@@ -1,3 +1,4 @@
+from model.logger import *
 
 class WikiDocument(object):
     def __init__(self, doc_id, title, raw_text, rev_id=None):
@@ -5,4 +6,17 @@ class WikiDocument(object):
         self.title = title
         self.raw_text = raw_text 
         self.rev_id = rev_id
+        DEBUG("Created WikiDocument: {}".format(self))
+    
+    def __str__(self):
+        retval = """
+            doc_id = {id} 
+            title = {title} 
+            raw_text = {raw_text}... 
+            rev_id = {rev_id}""".format(
+                   id=self.id, 
+                   title=self.title,
+                   raw_text=self.raw_text[:70], 
+                   rev_id=self.rev_id)
+        return retval 
     
