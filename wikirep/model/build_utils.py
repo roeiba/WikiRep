add complex tests, fix performance in normalization, fix performance in td-idf index building

wikirep/model/build_utils.py
@@ -5,9 +5,10 @@ Created on Oct 4, 2012
 '''
 import scipy as np
 from math import log
-from scipy.sparse import csr_matrix as matrix
+from scipy.sparse import csr_matrix, dok_matrix
 
 import logging
+from scipy.sparse.dok import dok_matrix
 _log = logging.getLogger(__name__)
 
 def build_index_by_words(word_list):
@@ -73,7 +74,7 @@ def build_wieght_table(df_vec,index_by_word,concepts_list):
     n = float(len(concepts_list))
     m = len(df_vec)
     assert int(m) != 0 and int(n) != 0
-    T = matrix((m,n))
+    T = csr_matrix((m,n))
     for j, concept in enumerate(concepts_list):
         words = concept.get_all_words()
         for word in words:
@@ -83,3 +84,25 @@ def build_wieght_table(df_vec,index_by_word,concepts_list):
             T[i,j] = tf_ij * log( n / df_vec[i] )
     return T 
 
+def build_wieght_table_dok(df_vec,index_by_word,concepts_list):
+    ''' 
+    First builds dok matrix
+    Creates not normalized Inverted Table as described on page 6 of 2009_full
+    @param df_vec: df_vec[i] - number of documents, in witch word with index i appered 
+    @param index_by_word: { word => index }
+    @param concepts_list
+    '''
+    
+    n = float(len(concepts_list))
+    m = len(df_vec)
+    assert int(m) != 0 and int(n) != 0
+    T = dok_matrix((m,n))
+    for j, concept in enumerate(concepts_list):
+        words = concept.get_all_words()
+        for word in words:
+            i =  index_by_word[word]
+            tf_ij = concept.get_tf(word)
+                
+            T[i,j] = tf_ij * log( n / df_vec[i] )
+    
+    return T.tocsr()

wikirep/model/db_builder.py
@@ -6,7 +6,7 @@ Created on Sep 28, 2012
 
 from concept import Concept 
 from model.database_wrapper import DatabaseWrapper
-from model.build_utils import build_word_index, build_index_by_words, build_df, build_wieght_table
+from model.build_utils import build_word_index, build_index_by_words, build_df, build_wieght_table, build_wieght_table_dok
 from scipy.sparse import csr_matrix as matrix
 from model.math_utils import normalize 
 
@@ -86,7 +86,7 @@ class DbBuilder(object):
         _log.info("DF vector build is DONE")
                     
         # weight table not normalized
-        T = build_wieght_table(df_vec, index_by_word, self.concepts_list)
+        T = build_wieght_table_dok(df_vec, index_by_word, self.concepts_list)
         _log.info("ID-TDF vector build is DONE")
                 
         if normalization:

wikirep/model/math_utils.py
@@ -1,5 +1,6 @@
 from scipy.linalg import norm
 from scipy.sparse import csr_matrix as matrix
+import scipy.sparse as sps
 from math import log
 
 from model.logger import getLogger
@@ -47,12 +48,33 @@ def normL2(T,row):
         s += T[i,l]*T[i,l]
     the_norm = s**0.5
     return the_norm
-    
-def normalize(T):   
+
+def normalize_regular(T):   
     n,m = T.shape
     for i in xrange(n):
         denom = normL2(T,i)
         if denom == 0.0: continue    
         for j in xrange(m): 
             T[i,j] = T[i,j]/ denom
+
+def normalize_csr(T):
+    """ Optimized for time version of normalize for csr matrix"""   
+    n,_ = T.shape
+    for i in xrange(n):
+        row_data = T.data[T.indptr[i]:T.indptr[i+1]]
+        denom =  (row_data**2).sum()**0.5
+        if denom == 0.0: continue 
+        T.data[T.indptr[i]:T.indptr[i+1]]= row_data / denom  
+
+def normalize(T):    
+    if isinstance(T, sps.csr_matrix):
+        return normalize_csr(T)
+    else:
+        return normalize_regular(T)
+    
+    
+    
+    
+    
+    
     